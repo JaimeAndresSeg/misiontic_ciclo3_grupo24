4b6b70f6110aff90a51c516370761e47e7a940e8
JaimeAndresSeg <andres.segurace@gmail.com>
2022-08-28 05:35:34 -0500

    Agregar lo que ya tenía en el otro repositorio

---
+++ intro-git/HolaMundo.java
@@ -0,0 +1,19 @@
+class HolaMundo{
+    public static void main(String[] args) {
+        
+    }
+
+    public static void message() {
+        System.out.println("HolaMundo");
+    }
+
+    public static int sumar(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public static int restar(int n1, int n2) {
+        return n1 - n2;
+    }
+
+
+}
